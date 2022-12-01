dda789ba78d4f652d1d4424b9eb6a31022d3da1c
akashp69 <akashpakhare499@gmail.com>
2022-12-01 23:26:08 +0530

    Added: Program For Displaying Welcome Message

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    /**
+     *This is Main Method is Used for Displaying welcome Message
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book ");
+    }
+}
